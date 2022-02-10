20b50ce06b67b01cc029be1485d425572e72bcc7
Stefan Rotaru <96879770+stefrotaru@users.noreply.github.com>
2022-02-10 13:32:09 +0200

    Added a css file and an images folder in which I'll store pictures for each individual 'news' article

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let ul = document.querySelector("ul");
 
 
 const stories = [
-	"Story 1",
+	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	"Story 2",
 	"Story 3",
 	"Story 4",
